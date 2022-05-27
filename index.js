3abcf79b74993fa9042899f0527ae0875ab4f9be
Md Naeem talukder <naeemtalukder169135@gmail.com>
2022-05-28 05:54:12 +0600

    admin put api

--- index.js
+++ index.js
@@ -51,21 +51,21 @@ async function run() {
         const userCollection = client.db('bike_user').collection('users');
 
         //GET user API
-        app.get('/user', async (req, res) => {
+        app.get('/user', verifyJWT, async (req, res) => {
             const users = await userCollection.find().toArray();
             res.send(users);
         });
 
-        // //PUT admin API
-        // app.put('/user/admin/:email', verifyJWT, async (req, res) => {
-        //     const email = req.params.email;
-        //     const filter = { email: email };
-        //     const updateDoc = {
-        //         $set: { role: 'admin' },
-        //     };
-        //     const result = await userCollection.updateOne(filter, updateDoc);
-        //     res.send(result);
-        // })
+        //PUT admin API
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
 
         //PUT users API
         app.put('/user/:email', async (req, res) => {

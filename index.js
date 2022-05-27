7d21ae7ede51498e052a6b5128f344080bce93ba
Md Naeem talukder <naeemtalukder169135@gmail.com>
2022-05-27 20:48:50 +0600

    env file change and collection change

--- index.js
+++ index.js
@@ -7,19 +7,29 @@ const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+app.use(
+    cors({
+        origin: true,
+        optionsSuccessStatus: 200,
+        credentials: true,
+    })
+);
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ao6h9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uiw7b.mongodb.net/?retryWrites=true&w=majority`;
-
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uiw7b.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect();
+        console.log('db connect');
         const productCollection = client.db('bikeMenufacture').collection('products');
-        const orderCollection = client.db('bikeMenufacture').collection('orders');
+
+        const orderCollection = client.db('bike_orders').collection('orders');
         const reviewCollection = client.db('bike_reviews').collection('reviews');
 
         // GET products API
